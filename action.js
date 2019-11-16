a0b8b474794a4a73b7ee28f1db8c21a6183f6fb8
Junhui Yang <yangjunhuiyjh@gmail.com>
2019-11-16 15:52:35 +0000

    Chrome extension initialisation

---
+++ action.js
@@ -0,0 +1,16 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+let shrink = document.getElementById('shrink');
+let convert = document.getElementById('convert');
+
+shrink.onclick = function(element) {
+    // TODO
+};
+
+convert.onclick = function(element) {
+    // TODO
+};
